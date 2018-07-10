e6d509cc456685cdff89dda0fca05d631ff234a3
Sharath chandra <Sharath.chandrag2@gmail.com>
2018-07-10 17:28:18 +0530

    Add files via upload

---
+++ Week-3/Day-1/simulate_5_sided_die.py
@@ -0,0 +1,17 @@
+def rand5():
+
+    # Implement rand5() using rand7()
+    rand = rand7()
+    return rand if rand <= 5 else rand5()
+
+print('Rolling 5-sided die...')
+print(rand5())
+
+lst = [0] * 5
+
+for i in range(10000):
+	lst[rand5()-1] += 1
+
+print(lst)
+
+# infinity
